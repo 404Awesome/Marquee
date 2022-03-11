80464a3bcc09251fc465dc24866511c772a103cc
百晓生 <awesome404@163.com>
2022-03-11 22:14:08 +0800

    add AlibabaFont

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <div></div>
+  <Marquee />
 </template>
 
 <script setup lang="ts">
-
+import Marquee from "./components/marquee/Marquee.vue"
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+@font-face {
+  font-family: "AlibabaPuHuiTi";
+  src: url("./assets/AlibabaPuHuiTi.ttf");
+}
+
+body {
+  font-family: "AlibabaPuHuiTi";
+}
 </style>
